b4e0739c94a515d9b640edd537b7a3f0e4ccdf63
lingtao-sa <83944109+lingtao-sa@users.noreply.github.com>
2022-03-07 17:55:08 +1100

    add init-windows-basic.ps1
    Most basic additional provisioning after creating Cloud PC

---
+++ init-windows-basic.ps1
@@ -0,0 +1,2 @@
+# Enable Choco - The preferred way to managed the software packages
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
