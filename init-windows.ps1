7370e8fd4d8c545b1a90a18f1aeed93cd17d8c67
lingtao-sa <83944109+lingtao-sa@users.noreply.github.com>
2022-03-07 15:16:47 +1100

    Update RSAT and enable WSL

--- init-windows.ps1
+++ init-windows.ps1
@@ -1,3 +1,4 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
-Install-WindowsFeature RSAT
+Add-WindowsCapability -Online -Name Rsat.RemoteAccess.Management.Tools~~~~0.0.1.0
+Add-WindowsCapability -Online -Name Rsat.RemoteDesktop.Services.Tools~~~~0.0.1.0
+wsl --install
